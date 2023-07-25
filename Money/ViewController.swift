6d839d1dd572c71f9f47be71447ef191f3f13189
dragosh0705 <pingu0705@hanyang.ac.kr>
2023-07-25 11:06:00 +0900

    Initial Commit

---
+++ Money/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money
+//
+//  Created by 최영우 on 7/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
